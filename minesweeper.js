7b059f15ca9251376eeb7bf4a23699eccaffe45c
Karishma Patel <KarishmaPatel93@hotmail.com>
2020-07-30 12:37:42 +1200

    initial

--- minesweeper.js
+++ minesweeper.js
@@ -1,32 +1,173 @@
-document.addEventListener('DOMContentLoaded', startGame)
+document.addEventListener("DOMContentLoaded", startGame);
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: [
+    {
+      row: 0,
+      col: 0,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 0,
+      col: 1,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 0,
+      col: 2,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 0,
+      col: 3,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
 
-function startGame () {
+    {
+      row: 1,
+      col: 0,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 1,
+      col: 1,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 1,
+      col: 2,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 1,
+      col: 3,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 2,
+      col: 0,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 2,
+      col: 1,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 2,
+      col: 2,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 2,
+      col: 3,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 3,
+      col: 0,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 3,
+      col: 1,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 3,
+      col: 2,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+    {
+      row: 3,
+      col: 3,
+      isMine: Math.random() >= 0.8,
+      isMarked: false,
+      hidden: true,
+    },
+  ],
+};
+
+function startGame() {
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
+  document.getElementById("reset").addEventListener("click", resetBoard);
   // Don't remove this function call: it makes the game work!
-  lib.initBoard()
+  lib.initBoard();
+  document.addEventListener("click", checkForWin);
+  document.addEventListener("contextmenu", checkForWin);
+}
+
+function resetBoard() {
+  location.reload();
 }
 
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
-function checkForWin () {
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+function checkForWin() {
+  var win = true;
+  for (let i = 0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine === true && board.cells[i].isMarked === false) {
+      win = false;
+    }
+    if (board.cells[i].isMine === false && board.cells[i].hidden === true) {
+      win = false;
+    }
+  }
+  if (win === true) {
+    lib.displayMessage("You win!");
+  }
 }
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
+// cells yourself! Just use `lib.getSurroundingCells`:
 //
 //   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
 //
-// It will return cell objects in an array. You should loop through 
+// It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines (cell) {
+function countSurroundingMines(cell) {
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col);
+  var count = 0;
+  for (var i = 0; i < surrounding.length; i++) {
+    if (surrounding[i].isMine === true) {
+      count++;
+    }
+  }
+  return count;
 }
-
